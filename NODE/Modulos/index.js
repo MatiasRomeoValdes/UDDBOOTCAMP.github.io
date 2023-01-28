27e966ce2e4774ef4a91264a46b397d90e0d72a2
MatiasRomeo <m.romeov@hotmail.com>
2023-01-28 12:52:11 -0300

    Commit tareas

---
+++ NODE/Modulos/index.js
@@ -0,0 +1,6 @@
+const operaciones = require( 'suma.js')
+
+operaciones.suma(10, 15)
+operaciones.log( operaciones.suma(10, 15));
+operaciones.division(25/2)
+operacinones.log( operaciones.division(25/2));
---
+++ NODE/Modulos/suma.js
@@ -0,0 +1,8 @@
+const suma = (a, b) => { return a + b;
+};
+
+const division = (a, b) => { 
+return Math.floor(a/b);
+};
+
+module.exports = {suma, division};
