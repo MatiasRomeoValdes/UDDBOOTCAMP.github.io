a29c2486c7a4fa58beed3bb4663c51e8eb71b202
MatiasRomeo <m.romeov@hotmail.com>
2023-02-01 00:12:27 -0300

    Cambios de Carpetas

--- NODE/Modulos/suma.js
+++
@@ -1,8 +0,0 @@
-const suma = (a, b) => { return a + b;
-};
-
-const division = (a, b) => { 
-return Math.floor(a/b);
-};
-
-module.exports = {suma, division};
--- TAREANODEBOOT/app.js
+++
@@ -1,1 +0,0 @@
-const calculadora = require( './calculadora')
